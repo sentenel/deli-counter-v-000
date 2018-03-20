cb2cdb0725d9564cc95f124729d7fdc4437ac199
Seth Wax <sethlwax@gmail.com>
2018-03-20 09:37:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,3 +7,8 @@ def line(deli)
     puts "The line is currently:" + deli.map.with_index{|name, index| " #{index + 1}. #{name}"}.join
   end
 end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
