71b13252a05ecec4c30401e2d5b09029e253687b
Seth Wax <sethlwax@gmail.com>
2018-03-20 09:33:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
     line_string = "The line is currently:" + deli.map.with_index{|name, index| " #{index}. #{name}"}.join
+    binding.pry
   end
 end
